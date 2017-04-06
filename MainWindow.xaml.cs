cb2f9cb709cb2851a5e73f2ce63f096615b70809
Chuang Yun Hao <pp8323127@gmail.com>
2017-04-06 14:49:19 +0800

    視窗拉寬，並新增label欄位顯示資料

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -248,6 +248,43 @@ namespace Microsoft.Samples.Kinect.FaceBasics
 
 
 
+
+
+     //---------CoordinateMappingBasics-WPF----------//
+
+        /// <summary>
+        /// Size of the RGB pixel in the bitmap
+        /// </summary>
+        private readonly int bytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+
+        /// <summary>
+        /// Reader for depth/color/body index frames
+        /// </summary>
+        private MultiSourceFrameReader multiFrameSourceReader = null;
+
+        /// <summary>
+        /// The size in bytes of the bitmap back buffer
+        /// </summary>
+        private uint bitmapBackBufferSize = 0;
+
+        /// <summary>
+        /// Intermediate storage for the color to depth mapping
+        /// </summary>
+        private DepthSpacePoint[] colorMappedToDepthPoints = null;
+
+        /// <summary>
+        /// Bitmap to display
+        /// </summary>
+        private WriteableBitmap bitmap = null;
+
+
+
+
+
+
+
+
+
         //使用 subscription key，Microsoft Face API Key
         private readonly IFaceServiceClient faceServiceClent =
             new FaceServiceClient("4b84a43021ee4799bb07ef07a1fe91f5");
@@ -443,6 +480,45 @@ namespace Microsoft.Samples.Kinect.FaceBasics
             // use the window object as the view model in this simple example
             this.DataContext = this;
 
+
+
+
+
+
+
+        //---------CoordinateMappingBasics-WPF----------//
+            this.multiFrameSourceReader = this.kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Depth | FrameSourceTypes.Color | FrameSourceTypes.BodyIndex);
+
+            //this.multiFrameSourceReader.MultiSourceFrameArrived += this.Reader_MultiSourceFrameArrived;
+
+            this.coordinateMapper = this.kinectSensor.CoordinateMapper;
+
+            FrameDescription depthFrameDescription = this.kinectSensor.DepthFrameSource.FrameDescription;
+
+            int depthWidth = depthFrameDescription.Width;
+            int depthHeight = depthFrameDescription.Height;
+
+            int colorWidth = colorFrameDescription.Width;
+            int colorHeight = colorFrameDescription.Height;
+
+            this.colorMappedToDepthPoints = new DepthSpacePoint[colorWidth * colorHeight];
+
+            this.bitmap = new WriteableBitmap(colorWidth, colorHeight, 96.0, 96.0, PixelFormats.Bgra32, null);
+            
+            // Calculate the WriteableBitmap back buffer size
+            this.bitmapBackBufferSize = (uint)((this.bitmap.BackBufferStride * (this.bitmap.PixelHeight - 1)) + (this.bitmap.PixelWidth * this.bytesPerPixel));
+
+
+
+
+
+
+
+
+
+
+
+
             // initialize the components (controls) of the window
             this.InitializeComponent();
         }
@@ -464,8 +540,19 @@ namespace Microsoft.Samples.Kinect.FaceBasics
             }
         }
 
+        /// <summary>
+        /// Gets the bitmap to display
+        /// </summary>
+        public ImageSource ImageSource_coor
+        {
+            get
+            {
+                return this.bitmap;
+            }
+        }
 
-        
+
+
 
         /// <summary>
         /// Gets the bitmap to display
@@ -478,7 +565,7 @@ namespace Microsoft.Samples.Kinect.FaceBasics
         //    }
         //}
 
-        
+
 
 
         /// <summary>
@@ -608,6 +695,15 @@ namespace Microsoft.Samples.Kinect.FaceBasics
                 this.kinectSensor.Close();
                 this.kinectSensor = null;
             }
+
+
+            if (this.multiFrameSourceReader != null)
+            {
+                // MultiSourceFrameReder is IDisposable
+                this.multiFrameSourceReader.Dispose();
+                this.multiFrameSourceReader = null;
+            }
+
 
         }
 
@@ -1271,6 +1367,7 @@ namespace Microsoft.Samples.Kinect.FaceBasics
                             DrawTextFontSize,
                             drawingBrush),
                         faceTextLayout);
+                label.Content = faceText;
             }
         }
 
@@ -1445,6 +1542,164 @@ namespace Microsoft.Samples.Kinect.FaceBasics
 
 
         }
+
+
+
+
+
+
+    //---------CoordinateMappingBasics-WPF----------//
+
+        /// <summary>
+        /// Handles the depth/color/body index frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            int depthWidth = 0;
+            int depthHeight = 0;
+
+            DepthFrame depthFrame = null;
+            ColorFrame colorFrame = null;
+            BodyIndexFrame bodyIndexFrame = null;
+            bool isBitmapLocked = false;
+
+            MultiSourceFrame multiSourceFrame = e.FrameReference.AcquireFrame();
+
+            // If the Frame has expired by the time we process this event, return.
+            if (multiSourceFrame == null)
+            {
+                return;
+            }
+
+            // We use a try/finally to ensure that we clean up before we exit the function.  
+            // This includes calling Dispose on any Frame objects that we may have and unlocking the bitmap back buffer.
+            try
+            {
+                depthFrame = multiSourceFrame.DepthFrameReference.AcquireFrame();
+                colorFrame = multiSourceFrame.ColorFrameReference.AcquireFrame();
+                bodyIndexFrame = multiSourceFrame.BodyIndexFrameReference.AcquireFrame();
+
+                // If any frame has expired by the time we process this event, return.
+                // The "finally" statement will Dispose any that are not null.
+                if ((depthFrame == null) || (colorFrame == null) || (bodyIndexFrame == null))
+                {
+                    return;
+                }
+
+                // Process Depth
+                FrameDescription depthFrameDescription = depthFrame.FrameDescription;
+
+                depthWidth = depthFrameDescription.Width;
+                depthHeight = depthFrameDescription.Height;
+
+                // Access the depth frame data directly via LockImageBuffer to avoid making a copy
+                using (KinectBuffer depthFrameData = depthFrame.LockImageBuffer())
+                {
+                    this.coordinateMapper.MapColorFrameToDepthSpaceUsingIntPtr(
+                        depthFrameData.UnderlyingBuffer,
+                        depthFrameData.Size,
+                        this.colorMappedToDepthPoints);
+                }
+
+                // We're done with the DepthFrame 
+                depthFrame.Dispose();
+                depthFrame = null;
+
+                // Process Color
+
+                // Lock the bitmap for writing
+                this.bitmap.Lock();
+                isBitmapLocked = true;
+
+                colorFrame.CopyConvertedFrameDataToIntPtr(this.bitmap.BackBuffer, this.bitmapBackBufferSize, ColorImageFormat.Bgra);
+
+                // We're done with the ColorFrame 
+                colorFrame.Dispose();
+                colorFrame = null;
+
+                // We'll access the body index data directly to avoid a copy
+                using (KinectBuffer bodyIndexData = bodyIndexFrame.LockImageBuffer())
+                {
+                    unsafe
+                    {
+                        byte* bodyIndexDataPointer = (byte*)bodyIndexData.UnderlyingBuffer;
+
+                        int colorMappedToDepthPointCount = this.colorMappedToDepthPoints.Length;
+
+                        fixed (DepthSpacePoint* colorMappedToDepthPointsPointer = this.colorMappedToDepthPoints)
+                        {
+                            // Treat the color data as 4-byte pixels
+                            uint* bitmapPixelsPointer = (uint*)this.bitmap.BackBuffer;
+
+                            // Loop over each row and column of the color image
+                            // Zero out any pixels that don't correspond to a body index
+                            for (int colorIndex = 0; colorIndex < colorMappedToDepthPointCount; ++colorIndex)
+                            {
+                                float colorMappedToDepthX = colorMappedToDepthPointsPointer[colorIndex].X;
+                                float colorMappedToDepthY = colorMappedToDepthPointsPointer[colorIndex].Y;
+
+                                // The sentinel value is -inf, -inf, meaning that no depth pixel corresponds to this color pixel.
+                                if (!float.IsNegativeInfinity(colorMappedToDepthX) &&
+                                    !float.IsNegativeInfinity(colorMappedToDepthY))
+                                {
+                                    // Make sure the depth pixel maps to a valid point in color space
+                                    int depthX = (int)(colorMappedToDepthX + 0.5f);
+                                    int depthY = (int)(colorMappedToDepthY + 0.5f);
+
+                                    // If the point is not valid, there is no body index there.
+                                    if ((depthX >= 0) && (depthX < depthWidth) && (depthY >= 0) && (depthY < depthHeight))
+                                    {
+                                        int depthIndex = (depthY * depthWidth) + depthX;
+
+                                        // If we are tracking a body for the current pixel, do not zero out the pixel
+                                        if (bodyIndexDataPointer[depthIndex] != 0xff)
+                                        {
+                                            continue;
+                                        }
+                                    }
+                                }
+
+                                bitmapPixelsPointer[colorIndex] = 0;
+                            }
+                        }
+
+                        this.bitmap.AddDirtyRect(new Int32Rect(0, 0, this.bitmap.PixelWidth, this.bitmap.PixelHeight));
+                    }
+                }
+            }
+            finally
+            {
+                if (isBitmapLocked)
+                {
+                    this.bitmap.Unlock();
+                }
+
+                if (depthFrame != null)
+                {
+                    depthFrame.Dispose();
+                }
+
+                if (colorFrame != null)
+                {
+                    colorFrame.Dispose();
+                }
+
+                if (bodyIndexFrame != null)
+                {
+                    bodyIndexFrame.Dispose();
+                }
+            }
+        }
+
+
+
+
+
+
+
+
 
 
 
